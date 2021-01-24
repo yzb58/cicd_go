7c77710f42ebd788eabc4431e6ac9b062e2bf03a
yeziibin <753494666@qq.com>
2021-01-24 12:57:03 +0800

    修改代码

--- main.go
+++ main.go
@@ -8,11 +8,7 @@ import (
 func main() {
 	tool.Echo("hello world")
 	nums := make([]int, 0)
-	nums[0] = 1
-	nums[1] = 3
-	nums[2] = 5
-	nums[3] = 8
-	nums[4] = 9
+	nums = append(nums, 1, 3, 5, 8, 9)
 	num := tool.Sum(nums)
 	fmt.Println(num)
 }
